b0c9b426672ff6588d9134f11b06b2ecb78169ba
Altun <a.ozder@outlook.com>
2021-04-14 14:55:51 -0400

    Maven

---
+++ src/main/java/appCodes/AppForTest.java
@@ -0,0 +1,16 @@
+package appCodes;
+
+public class AppForTest {
+    public int sumNbrs(int a, int b) {
+        return a+b+1;
+    }
+
+    public String addString(String a, String b) {
+        return a + "_"+b; //String1_String2
+    }
+
+    public int[] getArray () {
+        int[] arrayEx = {1, 2, 3};
+        return arrayEx;
+    }
+}
---
+++ src/test/java/Command.java
@@ -0,0 +1,12 @@
+public class Command {
+//    mvn clean --> clears target folder
+//
+//mvn clean test --> executes all classes named with Test at the end
+//
+//mvn clean verify -PcontrolSuite --> executes specific maven profile holding
+// details of test suite xmls; in this example profile id is controlSuite
+//
+//mvn clean verify -PcontrolSuite -Ddate=5 --> executes specific maven profile
+// holding details of test suite xmls; -D sets System property and executes
+// System.setProperty("date","5") in the background
+}
